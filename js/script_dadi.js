9c79f161da91614f903799cb39a23d0ebd167bb4
Luca Isidoro <apprendista44@gmail.com>
2020-05-25 16:07:02 +0200

    Consegna esercizi

---
+++ js/script_dadi.js
@@ -0,0 +1,17 @@
+//variabili con valore dado
+var punteggioGiocatore = Math.floor(Math.random() *6) +1;
+var punteggioPc = Math.floor(Math.random() *6) +1;
+
+//mi aggancio all'html
+var risposta = document.getElementById('risposta');
+
+//valuto i casi
+if (punteggioGiocatore == punteggioPc ) {
+  risposta.innerHTML = 'Giocatore: ' +  punteggioGiocatore + ' Computer: ' + punteggioPc +' Pareggio!';
+} else if (punteggioGiocatore >= punteggioPc) {
+  risposta.innerHTML = 'Giocatore: ' +  punteggioGiocatore + ' Computer: ' + punteggioPc + ' Hai vinto!';
+
+}else if (punteggioGiocatore <= punteggioPc) {
+  risposta.innerHTML = 'Giocatore: ' +  punteggioGiocatore + ' Computer: ' + punteggioPc + ' Hai perso...';
+
+}
